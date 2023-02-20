f600dd16b19910f99749d674da3ff227f6e04a70
angel <angelgmorenor@gmail.com>
2023-02-20 00:27:47 -0400

    Cambios en constructor de sujetos_estudio, reporte_med se aplicoPOO

--- modelos/sujetos_estudio.py
+++ modelos/sujetos_estudio.py
@@ -1,19 +1,19 @@
 class Sujetos_Estudio:
-     def __init__(self):
-        self.id_sujeto
-        self.nombres
-        self.apellidos
-        self.fecha_nacimiento
-        self.tipo_documento
-        self.codigo_documento
-        self.sexo
-        self.genero
-        self.orientacion_sexual
-        self.nacionalidad
-        self.provincia
-        self.correo
-        self.fecha_creacion
-        self.condiciones_medicas = []
+     def __init__(self, id_sujeto, nombres, apellidos, fecha_nacimiento, tipo_documento, codigo_documento, sexo, genero, orientacion_sexual, nacionalidad, provincia, correo, fecha_creacion, condiciones_medicas):
+        self.id_sujeto = id_sujeto
+        self.nombres = nombres
+        self.apellidos = apellidos
+        self.fecha_nacimiento = fecha_nacimiento
+        self.tipo_documento = tipo_documento
+        self.codigo_documento = codigo_documento
+        self.sexo = sexo
+        self.genero = genero
+        self.orientacion_sexual = orientacion_sexual
+        self.nacionalidad = nacionalidad
+        self.provincia = provincia
+        self.correo = correo
+        self.fecha_creacion = fecha_creacion
+        self.condiciones_medicas = condiciones_medicas
         
      def ingresar(self, codigo_documento, tipo_documento):
          return bool
