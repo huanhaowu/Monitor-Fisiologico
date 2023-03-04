ae74c5472ad214cc09d9a535540506a850d25700
Pazzis Paulino <1103790@est.intec.edu.do>
2023-03-03 23:42:57 -0400

    Clase de conexion lista

---
+++ scripts.sql
@@ -0,0 +1,147 @@
+--CREACION DE LAS TABLAS
+CREATE TABLE Usuarios(
+    id_usuario INTEGER PRIMARY KEY AUTOINCREMENT,
+    id_tipo_usuario INTEGER NOT NULL,
+    nombre TEXT NOT NULL,
+    apellido TEXT NOT NULL,
+    fecha_nacimiento DATE NOT NULL,
+    usuario Text NOT NULL,
+    contrasena TEXT NOT NULL,
+    ID_Creador INTEGER NOT NULL,
+    correo_electronico TEXT NOT NULL,
+    telefono TEXT NOT NULL,
+    activo TEXT NOT NULL,
+    fecha_creacion DATE NOT NULL,
+    FOREIGN KEY (id_tipo_usuario)
+    REFERENCES Tipo_Usuario(id_tipo_usuario),
+    FOREIGN KEY (ID_Creador)
+    REFERENCES Usuarios(id_usuario)
+);
+
+CREATE TABLE Orientacion_Sexual(
+    id_orientacion_sexual INTEGER PRIMARY KEY AUTOINCREMENT,
+    descripcion TEXT NOT NULL
+);
+
+CREATE TABLE Nacionalidad(
+    id_nacionalidad INTEGER PRIMARY KEY AUTOINCREMENT,
+    descripcion TEXT NOT NULL
+);
+
+CREATE TABLE Provincia(
+    id_provincia INTEGER PRIMARY KEY AUTOINCREMENT,
+    descripcion TEXT NOT NULL
+);
+
+CREATE TABLE Sexo(
+    id_sexo INTEGER PRIMARY KEY AUTOINCREMENT,
+    descripcion TEXT NOT NULL
+);
+
+CREATE TABLE Tipo_Documento(
+    id_tipo_documento INTEGER PRIMARY KEY AUTOINCREMENT,
+    descripcion TEXT NOT NULL
+);
+
+CREATE TABLE Condiciones_Sujeto(
+    id_condiciones_sujeto INTEGER PRIMARY KEY AUTOINCREMENT,
+    id_sujeto INTEGER NOT NULL,
+    id_condicion_medica INTEGER NOT NULL,
+    FOREIGN KEY (id_sujeto)
+    REFERENCES Sujetos_estudio(id_sujeto),
+    FOREIGN KEY (id_condicion_medica)
+    REFERENCES Condiciones_Medicas(id_condicion_medica)
+);
+
+CREATE TABLE Condiciones_Medicas(
+    id_condicion_medica INTEGER PRIMARY KEY AUTOINCREMENT,
+    descripcion TEXT NOT NULL
+);
+
+CREATE TABLE Condicion_Parametro(
+    id_condicion_parametro INTEGER PRIMARY KEY AUTOINCREMENT,
+    id_condicion_medica INTEGER NOT NULL,
+    id_parametro_fisiologico INTEGER NOT NULL,
+    FOREIGN KEY (id_condicion_medica)
+    REFERENCES Condiciones_Medicas(id_condicion_medica),
+    FOREIGN KEY (id_parametro_fisiologico)
+    REFERENCES Parametros_fisiologicos(id_parametro_fisiologico)
+);
+
+CREATE TABLE Tipo_Usuario(
+    id_tipo_usuario INTEGER PRIMARY KEY AUTOINCREMENT,
+    descripcion TEXT NOT NULL
+);
+
+CREATE TABLE historico_solicitud(
+    id_historico_solicitud INTEGER PRIMARY KEY AUTOINCREMENT,
+    id_usuario INTEGER NOT NULL,
+    fecha_solicitud DATE NOT NULL,
+    descripcion TEXT NOT NULL,
+    FOREIGN KEY (id_usuario)
+    REFERENCES Usuarios(id_usuario)
+    );
+
+CREATE TABLE Mediciones_sujeto (
+    id_medicio INTEGER PRIMARY KEY AUTOINCREMENT,
+    id_sujeto INTEGER NOT NULL,
+    peso_sujeto REAL NOT NULL,
+    altura_sujeto REAL NOT NULL,
+    fecha_medicion DATETIME NOT NULL,
+    FOREIGN KEY (id_sujeto) 
+    REFERENCES Sujetos_estudio(id_sujeto)
+);
+
+CREATE TABLE Medicion_parametro (
+    id_detalle_medicion INTEGER PRIMARY KEY AUTOINCREMENT,
+    id_medicion INTEGER NOT NULL,
+    id_parametro_fisiologico INTEGER NOT NULL,
+    medida_parametro_fisiologico REAL NOT NULL,
+    FOREIGN KEY (id_medicion)
+    REFERENCES Mediciones_sujeto(id_medicion),
+    FOREIGN KEY (id_parametro_fisiologico)
+    REFERENCES Parametros_fisiologicos(id_parametro_fisiologico)
+);
+
+CREATE TABLE Parametros_fisiologicos (
+    id_parametro_fisiologico INTEGER PRIMARY KEY AUTOINCREMENT,
+    descripcion TEXT NOT NULL,
+    min_estandar REAL NOT NULL,
+    max_estandar REAL NOT NULL,
+    alerta_bajo REAL,
+    alerta_alto REAL,
+    critico_bajo REAL,
+    critico_alto REAL
+);
+
+CREATE TABLE Genero (
+    id_genero INTEGER PRIMARY KEY AUTOINCREMENT,
+    descripcion TEXT NOT NULL
+);
+
+CREATE TABLE Sujetos_estudio (
+    id_sujeto INTEGER PRIMARY KEY AUTOINCREMENT,
+    nombre TEXT NOT NULL,
+    apellido TEXT NOT NULL,
+    fecha_nacimiento DATE NOT NULL,
+    id_tipo_documento INTEGER NOT NULL,
+    codigo_documento TEXT NOT NULL,
+    id_sexo INTEGER NOT NULL,
+    id_genero INTEGER NOT NULL,
+    id_orientacion_sexual INTEGER NOT NULL,
+    id_nacionalidad INTEGER NOT NULL,
+    provincia TEXT NOT NULL,
+    correo_electronico TEXT NOT NULL,
+    FOREIGN KEY (id_tipo_documento)
+    REFERENCES Tipo_documento(id_tipo_documento),
+    FOREIGN KEY (id_sexo)
+    REFERENCES Sexo(id_sexo),
+    FOREIGN KEY (id_genero)
+    REFERENCES Genero(id_genero),
+    FOREIGN KEY (id_orientacion_sexual)
+    REFERENCES Orientacion_sexual(id_orientacion_sexual),
+    FOREIGN KEY (id_nacionalidad)
+    REFERENCES Nacionalidad(id_nacionalidad)
+);
+
+-- INSERCION DE LOS DATOS INICIALES
