59f5ab0013895ccc4c025ddb0ea8b9c9c62bf262
Luis Adames <adames1601@gmail.com>
2023-03-05 17:36:35 -0400

    arreglos metodo ingresar sujeto

--- modelos/sujetos_estudio.py
+++ modelos/sujetos_estudio.py
@@ -7,7 +7,8 @@ from modelos.nacionalidad import Nacionalidad
 from modelos.provincia import Provincia
 from modelos.condiciones_medicas import CondicionesMedicas
 class SujetosEstudio:
-     def __init__(self, tipo_documento, codigo_documento, id_sujeto = 0, nombres = "", apellidos = "", fecha_nacimiento = None, sexo = None, genero = None, orientacion_sexual = None, nacionalidad = None, provincia = None, correo = "", fecha_creacion = None, condiciones_medicas = []):
+    
+    def __init__(self, tipo_documento, codigo_documento, id_sujeto = 0, nombres = "", apellidos = "", fecha_nacimiento = None, sexo = None, genero = None, orientacion_sexual = None, nacionalidad = None, provincia = None, correo = "", fecha_creacion = None, condiciones_medicas = []):
         self.id_sujeto = id_sujeto
         self.nombres = nombres
         self.apellidos = apellidos
@@ -25,7 +26,7 @@ class SujetosEstudio:
         
         
         
-     def ingresar(self, codigo_documento, tipo_documento):
+    def ingresar(self, codigo_documento, tipo_documento):
         bd = Conexion()
         resultado = bd.execute_query('''SELECT id_tipo_documento, codigo_documento
                             FROM Sujetos_Estudio
@@ -37,7 +38,7 @@ class SujetosEstudio:
         else:
             return False
      
-     def registrar(self, nombres, apellidos, fecha_nacimiento, tipo_documento, codigo_documento, sexo, genero, orientacion_sexual, nacionalidad, provincia, correo, condiciones_medicas):
+    def registrar(self, nombres, apellidos, fecha_nacimiento, tipo_documento, codigo_documento, sexo, genero, orientacion_sexual, nacionalidad, provincia, correo, condiciones_medicas):
         bd = Conexion()
         self.nombres = nombres
         self.apellidos = apellidos
@@ -50,19 +51,33 @@ class SujetosEstudio:
         self.nacionalidad = nacionalidad
         self.provincia = provincia
         self.correo = correo
-        self.condiciones_medicas = condiciones_medicas 
+        self.condiciones_medicas = condiciones_medicas
         
-        bd.execute_command('''INSERT INTO Sujetos_Estudio(nombre, apellido, fecha_nacimiento, id_tipo_documento, codigo_documento, id_sexo, id_genero, id_orientacion_sexual, id_nacionalidad, id_provincia, correo_electronico) 
-                              VALUES (?,?,?,?,?,?,?,?,?,?,?)''',
-                              [self.nombres, self.apellidos, self.fecha_nacimiento, self.tipo_documento.id_tipo_documento, self.codigo_documento, self.sexo.id_sexo, self.genero.id_genero, self.orientacion_sexual.id_orientacion_sexual, self.nacionalidad.id_nacionalidad, self.provincia.id_provincia, self.correo])
+        #Primero verifico si el usuario existe
         
-        self.id_sujeto = int(bd.execute_query("SELECT MAX(id_sujeto) FROM Sujetos_Estudio")[0][0])
-        if self.id_sujeto:
+        existe = bd.execute_query('''SELECT * FROM Sujetos_Estudio WHERE id_tipo_documento = ? AND codigo_documento = ?''', 
+                                  [self.tipo_documento.id_tipo_documento, self.codigo_documento])
+        
+        if existe:
+            self.id_sujeto = existe[0][0]
             for parametro in condiciones_medicas:
                 bd.execute_command(
                     '''INSERT INTO Condiciones_Sujeto (id_sujeto, id_condicion_medica) VALUES (?,?)''',
                     [self.id_sujeto, parametro.parametro.id_condicion_medicas]
                 )
+        else:
+            bd.execute_command('''INSERT INTO Sujetos_Estudio(nombre, apellido, fecha_nacimiento, id_tipo_documento, codigo_documento, id_sexo, id_genero, id_orientacion_sexual, id_nacionalidad, id_provincia, correo_electronico) 
+                              VALUES (?,?,?,?,?,?,?,?,?,?,?)''',
+                              [self.nombres, self.apellidos, self.fecha_nacimiento, self.tipo_documento.id_tipo_documento, self.codigo_documento, self.sexo.id_sexo, self.genero.id_genero, self.orientacion_sexual.id_orientacion_sexual, self.nacionalidad.id_nacionalidad, self.provincia.id_provincia, self.correo])
+        
+            self.id_sujeto = int(bd.execute_query("SELECT MAX(id_sujeto) FROM Sujetos_Estudio")[0][0])
+            if self.id_sujeto:
+                for parametro in condiciones_medicas:
+                    bd.execute_command(
+                        '''INSERT INTO Condiciones_Sujeto (id_sujeto, id_condicion_medica) VALUES (?,?)''',
+                        [self.id_sujeto, parametro.parametro.id_condicion_medicas]
+                    )
+        
      
      #def agregar_condiciones(self, condicion):
         #return 0
