98404d20d571b01418aede3c9835d80d490c42d8
Pazzis Paulino <1103790@est.intec.edu.do>
2023-03-08 16:42:46 -0400

    Metodo de cargar los datos del parametro indicado agregado

--- modelos/parametros_fisiologicos.py
+++ modelos/parametros_fisiologicos.py
@@ -11,6 +11,20 @@ class ParametrosFisiologicos:
         self.critico_alto = critico_alto
         self.critico_bajo = critico_bajo
         self.instrucciones = instrucciones
+
+    def cargar_datos_parametro(self, descripcion:str):
+        bd = Conexion()
+        resultado = bd.execute_query("SELECT * FROM Parametros_Fisiologicos WHERE descripcion = ?", [descripcion])
+        if resultado:
+            self.id_parametro_fisiologico = resultado[0][0]
+            self.descripcion = resultado[0][1]
+            self.min_estandar = resultado[0][2]
+            self.max_estandar = resultado[0][3]
+            self.alerta_alto = resultado[0][4]
+            self.alerta_bajo = resultado[0][5]
+            self.critico_alto = resultado[0][6]
+            self.critico_bajo = resultado[0][7]
+            self.instrucciones = resultado[0][8]
     
     def obtener_lista_parametros_fisiologicos(self):
         bd = Conexion()
