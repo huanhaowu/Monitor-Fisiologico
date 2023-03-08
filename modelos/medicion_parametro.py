84b6a0eda455ff0e18b2f2888432685b51d3a773
angel <angelgmorenor@gmail.com>
2023-03-08 17:37:25 -0400

    en reporte_med metodo llenar_barras creado y funcional. cambios en buscar_parametro

--- modelos/medicion_parametro.py
+++ modelos/medicion_parametro.py
@@ -9,30 +9,30 @@ class MedicionParametro:
         # '' normal, + alto, - bajo
         # amarillo -> #FFBF00 ; verde -> #00C040 ; rojo -> #FF0211 ; gris -> #F5F5F5
         if self.medida_parametro_fisiologico <= self.parametro.critico_bajo:
-            return ("#F5F5F5", '', self.medida_parametro_fisiologico)
+            return ("gris", '', self.medida_parametro_fisiologico)
 
         if self.parametro.critico_bajo < self.medida_parametro_fisiologico < self.parametro.alerta_bajo:
-            return ("#FF0211", '-', self.medida_parametro_fisiologico)
+            return ("rojo", '-', self.medida_parametro_fisiologico)
 
         if self.parametro.alerta_bajo <= self.medida_parametro_fisiologico < self.parametro.min_estandar:
-            return ("#FFBF00", '-', self.medida_parametro_fisiologico)
+            return ("amarillo", '-', self.medida_parametro_fisiologico)
 
         if self.parametro.min_estandar <= self.medida_parametro_fisiologico <= self.parametro.max_estandar:
-            return ("#00C040", '', self.medida_parametro_fisiologico)
+            return ("verde", '', self.medida_parametro_fisiologico)
 
         if (self.parametro.alerta_alto):
             if self.parametro.max_estandar < self.medida_parametro_fisiologico <= self.parametro.alerta_alto:
-                return ("#FFBF00", '+', self.medida_parametro_fisiologico)
+                return ("amarillo", '+', self.medida_parametro_fisiologico)
 
             if (self.parametro.critico_alto):
                 if self.parametro.alerta_alto < self.medida_parametro_fisiologico <= self.parametro.critico_alto:
-                    return ("#FF0211", '+', self.medida_parametro_fisiologico)
+                    return ("rojo", '+', self.medida_parametro_fisiologico)
 
                 if self.parametro.critico_alto < self.medida_parametro_fisiologico:
-                    return ("#FF0211", '', self.medida_parametro_fisiologico)
+                    return ("rojo", '', self.medida_parametro_fisiologico)
             else:
                 if self.parametro.alerta_alto < self.medida_parametro_fisiologico:
-                    return ("#FF0211", '+', self.medida_parametro_fisiologico)
+                    return ("rojo", '+', self.medida_parametro_fisiologico)
         else:
             if self.parametro.max_estandar < self.medida_parametro_fisiologico:
-                return ("#F5F5F5", '', self.medida_parametro_fisiologico)
+                return ("gris", '', self.medida_parametro_fisiologico)
