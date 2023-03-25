738f89e5365f2b73f9e41640503655790f7049b0
Axel Cabral <amcv07@gmail.com>
2023-03-25 11:42:24 -0400

    Parte 1 de cambios a login suj
    Comprobacion del codigo documento antes de pasar a la siguiente ventana. Requiere de string de 9 o mas caracteres  y solo acepta valores alfanumericos

--- interfaces/login_suj.py
+++ interfaces/login_suj.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from tkinter import Tk, ttk, Canvas, Entry, Button, PhotoImage, messagebox
+from tkinter import Tk, ttk, Canvas, Entry, Button, PhotoImage, messagebox, StringVar
 from modelos.tipo_documento import TipoDocumento as td
 
 OUTPUT_PATH = Path(__file__).parent
@@ -14,10 +14,11 @@ class LoginSujEstudio():
         self.window.geometry("1260x725+{}+{}".format(self.window.winfo_screenwidth() // 2 - 1260 // 2, self.window.winfo_screenheight() // 2 - 725 // 2))
         self.window.configure(bg = "#FFFFFF")
         self.window.title("Login Sujetos de Estudio")
-        #Arreglo - Documenta esta parte para que sea mas facil encontrar los campos, puedes guiarte de la estructura que siguieron los de la interfaz registro_suj.py
         #Arreglo - Agrupa los controles del formulario en secciones por tipo, es decir, pon todos los botones en un solo lado, todos los textos en otro. Esto con el objetivo facilitar los arreglos
         #Arreglo - Usa los "region" para definir las secciones de los controles
-        #Canvas
+
+        #INICIALIZACION DE VARIABLES
+        #Inicializando canvas principal de la ventana
         self.canvas = Canvas(
             self.window,
             bg = "#FFFFFF",
@@ -28,6 +29,44 @@ class LoginSujEstudio():
             relief = "ridge"
         )
 
+        #Imagen Boton Acceder
+        self.imagen_btn_acceder = PhotoImage(file=relative_to_assets("button_1.png"))
+
+        #Inicializando boton "Acceder"
+        self.btn_acceder = Button(
+            image=self.imagen_btn_acceder,
+            borderwidth=0,
+            highlightthickness=0,
+            command=lambda: self.abrir_registro(self.cb_tipo_doc.get(), self.txb_codigo_doc.get()),
+            relief="flat",
+            bg = "white"
+        )
+
+        #Imagen Logo de la Aplicación
+        self.imagen_logo_app = PhotoImage(file=relative_to_assets("image_1.png"))
+
+        #Inicializando variable para obtener lista de tipo_documento permitidos
+        tipo_documento = td()
+        lista_tipo_documento = tipo_documento.obtener_lista_tipo_documento()
+
+        #Inicializacion Combobox para el tipo de documento
+        self.cb_tipo_doc = ttk.Combobox(
+            state = "readonly",
+            value = lista_tipo_documento,
+            font = ("RobotoRoman Regular", 25 * -1)
+        )
+
+        #Inicializacion del textbox para el numero de documento
+        self.txb_codigo_doc = Entry(
+            bd=1,
+            bg="#FFFFFF",
+            fg="#000716",
+            highlightthickness=0,
+            font = ("RobotoRoman Regular", 25 * -1)
+        )
+
+        #UBICACIONES
+        #Ubicacion del canvas principal
         self.canvas.place(x = 0, y = 0)
         self.canvas.create_rectangle(
             0.0,
@@ -38,6 +77,39 @@ class LoginSujEstudio():
             outline=""
         )
 
+        #Ubicacion del logo de la aplicación
+        self.logo_app = self.canvas.create_image(
+            656.0,
+            113.0,
+            image=self.imagen_logo_app
+        )
+
+        #Ubicacion del boton acceder
+        self.btn_acceder.place(
+            x=376.0,
+            y=517.0,
+            width=493.0,
+            height=59.0
+        )
+
+         #Ubicacion del ComboBox para el tipo de documento
+        self.cb_tipo_doc.place(
+            x=421.0,
+            y=298.0,
+            width=404.0,
+            height=52.0
+        )
+        
+        #Ubicacion del textbox para el codigo de documento
+        self.txb_codigo_doc.place(
+            x=420.0,
+            y=416.0,
+            width=404.0,
+            height=52.0
+        )
+
+        #TEXTOS
+        #Pregunta acerca del tipo de documento
         self.canvas.create_text(
             453.0,
             255.0,
@@ -47,6 +119,7 @@ class LoginSujEstudio():
             font=("RobotoRoman Regular", 25 * -1)
         )
 
+        #Solicitud para ingresar el numero de documento
         self.canvas.create_text(
             421.0,
             376.0,
@@ -56,6 +129,7 @@ class LoginSujEstudio():
             font=("RobotoRoman Regular", 25 * -1)
         )
 
+        #Texto extra
         self.canvas.create_text(
             255.0,
             662.0,
@@ -64,82 +138,20 @@ class LoginSujEstudio():
             fill="#FFFFFF",
             font=("Inter", 25 * -1)
         )
-
-        #Imagen Boton Acceder
-        self.imagen_btn_acceder = PhotoImage(file=relative_to_assets("button_1.png"))
-        #Prppieades del boton acceder
-        self.btn_acceder = Button(
-            image=self.imagen_btn_acceder,
-            borderwidth=0,
-            highlightthickness=0,
-            command=lambda: self.abrir_registro(self.cb_tipo_doc.get(), self.txb_codigo_doc.get()),
-            relief="flat",
-            bg = "white"
-        )
-        #Localización del boton acceder
-        self.btn_acceder.place(
-                x=376.0,
-                y=517.0,
-                width=493.0,
-                height=59.0
-            )
-        #Imagen Logo de la Aplicación
-        self.imagen_logo_app = PhotoImage(file=relative_to_assets("image_1.png"))
-        #Localización del logo de la aplicación
-        self.logo_app = self.canvas.create_image(
-            656.0,
-            113.0,
-            image=self.imagen_logo_app
-        )
-
-        #Inicializando variable para obtener lista de tipo_documento permitidos
-        tipo_documento = td()
-        lista_tipo_documento = tipo_documento.obtener_lista_tipo_documento()
-
-        #Aqui va el ComboBox para el tipo de documento
-        self.cb_tipo_doc = ttk.Combobox(
-            state = "readonly",
-            value = lista_tipo_documento,
-            font = ("RobotoRoman Regular", 25 * -1)
-        )
-
+        
+        #PROCESOS
         #Provocando que el documento inicial sea la cedula
         self.cb_tipo_doc.current(0)
-
-        #Locación del ComboBox para el tipo de documento
-        self.cb_tipo_doc.place(
-            x=421.0,
-            y=298.0,
-            width=404.0,
-            height=52.0
-        )
         
-        #Aqui va el textbox para el codigo de documetno
-        self.txb_codigo_doc = Entry(
-            bd=1,
-            bg="#FFFFFF",
-            fg="#000716",
-            highlightthickness=0,
-            font = ("RobotoRoman Regular", 25 * -1)
-        )
-        
-        #Localización del textbox para el codigo de documento
-        self.txb_codigo_doc.place(
-            x=420.0,
-            y=416.0,
-            width=404.0,
-            height=52.0
-        )
-
         self.window.resizable(False, False)
         self.window.mainloop()
         
     def abrir_registro(self, id_tipo_documento:int, descripcion:str):
         from interfaces.registro_suj import RegistroSujeto as rs
-        #Arreglo - Trata de que los nombres de tus variables sean autodescriptivas, cambia el nombre bol
-        bol = comprobar_cod_documento(descripcion)
-        if bol == True:
+        codigo_aceptado = self.comprobar_cod_documento(descripcion)
+        if codigo_aceptado == True:
             self.window.destroy()
+            descripcion = descripcion.upper()
             rs(id_tipo_documento,descripcion)
         else:
             #Creacion de canva cuando el codigo de documento esté vacío
@@ -173,12 +185,12 @@ class LoginSujEstudio():
                 justify= 'center',
             )
             messagebox.showwarning("Rellene el formulario", "Ingrese el código de su documento")
+
+    # Función para comprobar que se pase un codigo de documento antes de cambiar de pantalla
+    def comprobar_cod_documento(self, codigo_documento:str):
+        if codigo_documento.isalnum() == True and len(codigo_documento) >= 9 and codigo_documento.find("ñ") == -1 and codigo_documento.find("Ñ") == -1:
+            return True
+        else:
+            return False
+                
         
-        
-# Función para comprobar que se pase un codigo de documento antes de cambiar de pantalla
-#Arreglo - Esta funcion debe ir dentro de la clase
-def comprobar_cod_documento(codigo_documento:str):
-    if codigo_documento == "":
-        return False
-    else:
-        return True
